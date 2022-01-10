c7834a49bfbbfa2ad3c707a36da696e1294ea58a
Jayerson <ayersonj@gmail.com>
2022-01-11 09:04:18 +1100

    dazed and confused

--- src/com/company/Card.java
+++ src/com/company/Card.java
@@ -1,13 +1,27 @@
 package com.company;
 
 public class Card {
-    String[] suit = {"♠","♡","♢","♣"};
-    String[] symbol = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
-    int[] value = {2,3,4,5,6,7,8,9,10,11,12,13,14};
 
-    Card(String[] suit, String[] symbol, int[] value) {
-        this.suit = suit;
-        this.symbol = symbol;
-        this.value = value;
+    String[] suits = {"♠","♡","♢","♣"};
+    String[] symbols = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+    int[] values = {2,3,4,5,6,7,8,9,10,11,12,13,14};
+
+    Card (String[] suits, String[] symbols, int[] values) {
+        for (int i = 0; i < 4; i++) {
+            String suit = suits[i];
+            for (int j = 0; j < 13; j++) {
+                String symbol = symbols[j];
+                int value = values[j];
+            }
+        }
     }
 }
+// What in the name of exasperation does this mean :
+//
+// "toString method describes class"
+
+// I gave up half way through part 3 at midnight
+//
+// The words don't make any sense
+//
+// I'm crying because my head hurts from banging it against the fucking wall
--- src/com/company/CardGame.java
+++ src/com/company/CardGame.java
@@ -1,12 +1,43 @@
 package com.company;
-import java.util.ArrayList;
+import java.util.*;
 
 public class CardGame {
     ArrayList<Card> deckOfCards = new ArrayList<Card>();
+    Card next;
+
+    public void sortBySuit() {
+
+    }
 
     public void getDeck() {
-        for (Card i : deckOfCards) {
-            System.out.println(i);
+        for (Card c : deckOfCards) {
+            System.out.println(c);
         }
     }
+
+    ArrayList<Card> shuffleDeck
+            (String[] suits, String[] symbols, int[] values)
+    {
+        for (int i = 0; i < 52; i++) {
+            int random = (int) Math.random() * 52 + 1;
+
+            String suit = suits[(int) random/13];
+            String symbol = symbols[(int) random/4];
+            int value = values[(int) random/4];
+
+            {suit, symbol, value};
+            // add this to the arraylist
+        }
+    } // or HOW does Collections.shuffle actually work
+
+    public Card dealCard() {
+        if (deckOfCards.iterator().hasNext()) {
+            next = deckOfCards.iterator().next();
+        } else { shuffleDeck(); dealCard(); }
+        return next;
+    }
+
+
 }
+
+// Collections.sort()
